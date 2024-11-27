691568481e784e7552277a1422136876f8401faf
ShernOC <sherlynea1994@hotmail.com>
2024-11-27 17:46:27 +0300

    file arrangement

---
+++ p1.py
@@ -0,0 +1,59 @@
+#Function that takes list of numbers returns new list 
+numbers = [1,2,4,45,56,78]
+def list_no(numbers):
+    for number in numbers:
+        return numbers
+    
+print(list_no(numbers))
+ 
+#2. function takes a list and return true if list is sorted in ascending order 
+
+def list_ascending(numbers):
+    if numbers == sorted(numbers):
+        return True
+    else: return False
+   
+print(list_ascending(numbers))
+
+
+#3 Function that prints numbers  not divisible by 7. 
+def print_No():
+    for numbers in range(0,30):
+        if numbers % 7 != 0: 
+            print(numbers)
+       
+print_No()
+
+# Create a Python program that calculates the maximum value among 5 numbers entered by the user. The program should:
+
+user = int(input("Enter 5 numbers:"))
+print (user)
+
+# Prompt the user to enter 5 numbers one by one through the terminal.
+
+def max_no_user():
+
+    pass 
+    
+
+# Find and display the maximum value among the entered numbers.
+# Ensure the input is numeric and handle invalid inputs gracefully (e.g., prompt the user again if a non-numeric value is entered).
+# Example 
+# Enter number 1: 12
+# Enter number 2: 45
+# Enter number 3: 7
+# Enter number 4: 89
+# Enter number 5: 34
+# The maximum value is: 89
+
+
+
+    
+
+
+
+
+    
+    
+    
+    
