5e35d79f374c4e97e987ac2d6beb4c02d384cd4b
ShernOC <sherlynea1994@hotmail.com>
2024-12-05 18:11:57 +0300

    initial wk2 code

--- Wk2_practice/one_many.py
+++ Wk2_practice/one_many.py
@@ -3,4 +3,48 @@
 # Create Department and Employee classes.
 # Implement methods in Department to add and remove employees.
 # Include attributes such as name and id for Employee and department_name for Department.
+
 # Task: Write a function to list all employees in a department and one to transfer an employee to another department.
+
+#Multiple employers 
+
+#Department class 
+class Department: 
+    empl_list = []
+    def __init__(self, department_name):
+        self.department_name = department_name
+    
+    #method add  
+    def add_employees(self,employee):
+        self.employee = []
+        self.employee.append(employee)
+
+    #method remove 
+    def remove_employees(self, employee):
+        self.employee.remove(employee)
+        
+    #created a class method
+    def list_of_all_employees(cls, employees):
+        
+    
+
+#Class employee 
+class Employee: 
+    def __init__(self, name, id):
+        self.name = name 
+        self.id = id 
+        
+         
+#Object 
+d = Department("Human Resource")
+
+#Object 
+emp1 = Employee("Maria", 45)
+emp5 = Employee("Kimbo",2 )
+
+#connect department with 
+
+
+
+
+    
--- Wk2_practice/one_one.py
+++ Wk2_practice/one_one.py
@@ -4,3 +4,48 @@
 # Define attributes like name for Author and title for Book.
 # Ensure that a Book object is always associated with an Author object.
 # Add a method to display the author’s name and their book title.
+
+
+#Calling an Author in relation to their book 
+class Author:
+    def __init__(self,name):
+        self.name = name 
+    
+class Book:
+    def __init__(self, title,author):
+        self. title = title
+        self.author = author
+        # self.author = author 
+        
+#Calling the Class 
+#Object of the Author
+a2= Author("Collins")
+# a1 = Author("Marion")
+
+
+#Object of the Book 
+b1 = Book("Things Fall Apart",a2)
+# b2 = Book("Inheritance Book")
+
+a2.name # Collins 
+b1.title # Things Falls apart 
+#creating a relationship 
+# print(f"The Author is {a2.name}: {b1.author.name}")
+# print(f"The title is {b1.title}: ")
+
+#calling the name of author and book title
+def display_name():
+    print(f"The author is: {b1.author.name} and the title is: {b1.title}")
+    
+display_name()
+
+
+
+
+
+
+
+        
+        
+        
+        
